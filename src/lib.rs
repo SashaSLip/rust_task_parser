8c18ae2250f4e134b0bcf95f1cfa8d5f5321bcac
SashaSLip <o.slipushkina@ukma.edu.ua>
2025-10-16 18:33:45 +0300

    Make list_parser public

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 peg::parser! {
-    grammar list_parser() for str {
+    pub grammar list_parser() for str {   
         rule number() -> u32
             = n:$(['0'..='9']+) {? n.parse().or(Err("u32")) }
 
